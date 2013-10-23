e5d078cc9ef91f0a4416656b2b9b312134489be6
Julio César Barrera Andrade <juliobarreraa@gmail.com>
2013-10-23 16:18:15 -0500

    Initial commit

---
+++ protected/migrations/m131023_210749_base_tables.php
@@ -0,0 +1,25 @@
+<?php
+
+class m131023_210749_base_tables extends CDbMigration
+{
+	public function up()
+	{
+	}
+
+	public function down()
+	{
+		echo "m131023_210749_base_tables does not support migration down.\n";
+		return false;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
